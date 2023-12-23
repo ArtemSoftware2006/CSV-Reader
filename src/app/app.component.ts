ce47b0f754f9b0df143230a55ec0c36b2d5634eb
Artem <artem.egorov.art2006@gmail.com>
2023-12-23 20:53:42 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'csv-reader';
+  title = 'CSV Reader';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CsvPanelPageComponent } from './pages/csv-panel-page/csv-panel-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginPageComponent},
+    {path: 'csv-panel', component: CsvPanelPageComponent}
+];
