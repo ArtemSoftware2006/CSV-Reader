d2f2a61e7fce47d0d507524d33cda3d82a9bb884
Artem <artem.egorov.art2006@gmail.com>
2023-12-23 20:54:08 +0300

    Добавил старницы логина и главную страницу

---
+++ src/app/pages/csv-panel-page/csv-panel-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-panel-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './csv-panel-page.component.html',
+  styleUrl: './csv-panel-page.component.scss'
+})
+export class CsvPanelPageComponent {
+
+}
