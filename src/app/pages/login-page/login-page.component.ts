d85bd852d119ddd7a78def1f719be0d05c8c03c0
Artem <artem.egorov.art2006@gmail.com>
2023-12-29 22:32:56 +0300

    Изменил страницу входа
    Добавил формы логина и регистрации и возможность переключения между входом и регистрацией

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../models/form/login-form/login-form.component';
+import { RegistrationFormComponent } from '../../models/form/registration-form/registration-form.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, RegistrationFormComponent, NgIf],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+  isLoginMode: boolean = true;
+  changeModeText = 'To registration'
+  changeMode() {
+    this.isLoginMode = !this.isLoginMode;
+    if (this.isLoginMode) {
+      this.changeModeText = 'To registration';
+    }
+    else {
+      this.changeModeText = "To login";
+    }
+  }
 }
