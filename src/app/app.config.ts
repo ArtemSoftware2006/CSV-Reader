4522b4f3a20fa7551c1d4bf43701b6328e5a21d1
Artem <artem.egorov.art2006@gmail.com>
2024-01-02 20:47:38 +0300

    Добавил interceptor для Authorization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,10 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthIterceptor } from './services/interceptors/auth-iterceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthIterceptor, multi: true }] 
 };
--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,33 +1,80 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
-import { Observable, Subject } from 'rxjs';
+import { catchError, map, Observable, Subject, throwError } from 'rxjs';
+import { IFile } from '../entity/file';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileService {
+  private readonly BASE_URL = 'http://localhost:5149/api'; 
   private selectedFile : File;
   private parser : NgxCsvParser = new NgxCsvParser();
   private parseCsvFile : Subject<Observable<any[] | NgxCSVParserError>> = new Subject<Observable<any[] | NgxCSVParserError>>();
 
-  constructor() { 
+  constructor(private httpClient : HttpClient) { 
     this.selectedFile = new File([], '');
   }
 
-  setFile(file : File) {
+  public setFile(file : File) {
     this.selectedFile = file;
     this.parseCsvFile.next(this.getCsvAsArray());
   }
 
-  getFile() : File {
+  public getFile() : File {
     return this.selectedFile
   }
 
-  getCsvAsArray() {
+  public getCsvAsArray() {
     return this.parser.parse(this.selectedFile, {header : false, delimiter : ',', encoding : "utf8"});
   }
 
-  getParseCsvFileSubject() : Subject<Observable<any[] | NgxCSVParserError>> {
+  public getParseCsvFileSubject() : Subject<Observable<any[] | NgxCSVParserError>> {
     return this.parseCsvFile;      
+  }
+
+  public saveFile(file : File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return new Promise<boolean> ((resolve, reject) => {
+      this.httpClient.post(this.BASE_URL + "/File/Create", formData)
+      .pipe(
+        catchError(error => {
+          if (error.status === 403 || error.status === 400) {
+            resolve(false);
+          }
+          return throwError(error);
+        }),
+        map((reuslt) => {
+          return true;
+        })
+      )
+      .subscribe((result) => {
+        resolve(result);
+      }, (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  public getAllFiles() {
+    return new Promise<IFile[]> ((resolve, reject) => {
+      this.httpClient.get<IFile[]>(this.BASE_URL + "/File/GetFiles")
+      .pipe(
+        catchError(error => {
+          return throwError(error);
+        }),
+        map((reuslt) => {
+          return reuslt;
+        })
+      )
+      .subscribe((result) => {
+        resolve(result);
+      }, (error) => {
+        reject(error);
+      });
+    });
   }
 }
