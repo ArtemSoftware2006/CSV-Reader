31d4325225b08a636025b8903a4df2892afce84a
Artem <artem.egorov.art2006@gmail.com>
2024-01-02 20:49:00 +0300

    Добавил route на page просмотра файлов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { CsvPanelPageComponent } from './pages/csv-panel-page/csv-panel-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AllFilesPageComponent } from './pages/all-files-page/all-files-page.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
     {path: 'csv-panel', component: CsvPanelPageComponent},
     {path: 'home', component: HomePageComponent},
+    {path: 'all-files', component: AllFilesPageComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: '**', redirectTo: 'error', pathMatch: 'full'},
 ];
--- src/app/pages/csv-panel-page/csv-panel-page.component.ts
+++ src/app/pages/csv-panel-page/csv-panel-page.component.ts
@@ -2,22 +2,23 @@ import { FileService } from './../../services/file.service';
 import { Component } from '@angular/core';
 import { CsvTableComponent } from '../../models/csv-table/csv-table.component';
 import { NgxCSVParserError } from 'ngx-csv-parser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-csv-panel-page',
   standalone: true,
-  imports: [CsvTableComponent],
+  imports: [CsvTableComponent, RouterLink],
   templateUrl: './csv-panel-page.component.html',
   styleUrl: './csv-panel-page.component.scss'
 })
 export class CsvPanelPageComponent {
   public data: any[] = [];
-
+  public fileSaved = false;
   constructor(private fileService : FileService) {
     
   }
   
-  onFileSelected(event: Event) {
+  public onFileSelected(event: Event) {
     const selectedElement = event.target as HTMLInputElement;
 
     if (selectedElement) {
@@ -43,5 +44,19 @@ export class CsvPanelPageComponent {
         }
       });
     }
+  }
+  public onSave() {
+    this.fileService.saveFile(this.fileService.getFile())
+    .then((result) => {
+      if (result) {
+        this.fileSaved = true; 
+        setTimeout(() => {
+          this.fileSaved = false;
+        }, 3000);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 }
